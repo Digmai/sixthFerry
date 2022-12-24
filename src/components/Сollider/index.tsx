84e069b4240ebac16d437e63dc5d457497634657
Digmai <qt20130@gmail.com>
2022-12-24 23:25:58 +0300

    Initial Commit

--- src/components/Сollider/index.tsx
+++ src/components/Сollider/index.tsx
@@ -1,17 +1,40 @@
+import { Items } from "../Items";
 import "./style.css";
 import { svgLevel_1, svgLevel_2, svgFile, svgCart } from "./svg";
 export const Сollider = () => {
+  const onMouseEdit = (
+    <div className="level_edit">
+      {svgFile}
+      {svgCart}
+    </div>
+  );
+  const APIarr = [{}, { children: [{}, {}] }];
+
+  const MapArr = (el: [{ children?: [] }]) => {
+    return (
+      <>
+        <Items />
+
+        {el?.map((e: any, i: any) => {
+          e.children?.length !== undefined
+            ? console.log("e.chi")
+            : console.log(e.children?.length, i);
+        })}
+      </>
+    );
+  };
+
   return (
     <div className="Сollider">
       <div className="topСollider">
         <div className="NameGroupsCollider">Строительно-монтажные работы</div>
       </div>
       <div className="titleСollider Box">
-        <table className="table">
+        <table className="table" cellSpacing="0">
           <tbody>
             <tr style={{ height: "42px" }}>
-              <th className="Level_1" style={{ width: "110px" }}>
-                <div className="Level"> Уровень</div>
+              <th style={{ width: "110px" }}>
+                <div className="Level">Уровень</div>
               </th>
               <th>
                 <div className="tableText">Наименование работ</div>
@@ -31,13 +54,10 @@ export const Сollider = () => {
             </tr>
             <tr style={{ height: "60px" }}>
               <td className="Level_1 " scope="row">
-                <div className="level_edit">
-                  {" "}
-                  {/*  this btn */}
-                  {svgLevel_1}
-                  {svgLevel_2}
-                  {svgFile}
-                  {svgCart}
+                <div className="svg_level_1">
+                  <div className="TopSvg"></div>
+                  <div> {svgFile}</div>
+                  <div className="BeamBottom"></div>
                 </div>
               </td>
               <td>
@@ -50,20 +70,8 @@ export const Сollider = () => {
                 <div className="tableText">level_edit</div>
               </td>
             </tr>
-            <tr style={{ height: "60px" }}>
-              <td className="Level_1 " scope="row">
-                <div className="EditIconLevel_2">{svgLevel_2}</div>
-              </td>
-              <td>
-                <div className="tableText">level_edit</div>
-              </td>
-              <td>
-                <div className="tableText">level_edit</div>
-              </td>
-              <td>
-                <div className="tableText">level_edit</div>
-              </td>
-            </tr>
+
+            <MapArr el={APIarr} />
           </tbody>
         </table>
       </div>
